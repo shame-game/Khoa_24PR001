06d9c237852002b79a92dd4c0bacdbe8ceabe647
shame-game <142651893+shame-game@users.noreply.github.com>
2024-05-18 16:17:59 +0700

    d

--- js/index.js
+++ js/index.js
@@ -3,54 +3,22 @@ const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
-/* vam-max */
-document.querySelector('.scroll-thongdiep-chuyendong').onclick = () => {
-    document.querySelector('.scroll-thongdiep-chuyendong>div').classList.toggle('on')
-    if (document.querySelector('.scroll-thongdiep-chuyendong>div.on') != null) {
-        document.querySelector('.scroll-thongdiep_content-chuyen h1').innerText = 'Các kỹ năng'
-        document.querySelector('.scroll-thongdiep_content-chuyen .scroll-thongdiep_content-exp1').innerText = 'Hoạt động trong cuộc thi giúp tạo cơ hội được tìm hiểu và luyện tập các kỹ năng tốt.'
-        document.querySelector('.scroll-thongdiep_content-chuyen .scroll-thongdiep_content-exp2').innerText = '* Nhấn để xem thêm các kỹ năng'
-        document.querySelector('.scroll-thongdiep_content1').innerHTML =
-            `<i class="bi bi-clipboard-check-fill"></i>
-            <h1>Kỹ năng toán học và vật lý</h1>
-        <p>Rèn luyện các kiến thức toán, lý trong việc tính toán khoảng cách, bài toán quỹ đạo chuyển động, v.v.</p>`
-        document.querySelector('.scroll-thongdiep_content2').innerHTML =
-            `<i class="bi bi-clipboard-check-fill"></i>
-        <h1>Kỹ năng thiết kế và chế tạo</h1>
-        <p>Rèn luyện việc hiện thực hóa các giải pháp, ý tưởng, đưa ra các sản phẩm mẫu.</p>`
-        document.querySelector('.scroll-thongdiep_content3').innerHTML =
-            ` <i class="bi bi-clipboard-check-fill"></i>
-        <h1>Kỹ năng lập trình cơ bản</h1>
-        <p>Sử dụng các ngôn ngữ lập trình phổ thông trên các thiết bị máy tính bảng/điện thoại, máy tính.</p>`
-        document.querySelector('.scroll-thongdiep_content4').innerHTML =
-            `<i class="bi bi-clipboard-check-fill"></i>
-        <h1>Kỹ năng quản lý công việc và làm việc nhóm</h1>
-        <p>Rèn luyện khả năng nhận định khối lượng công việc, phân công, xây dựng dự án đạt được hiệu quả cao.</p>`
-    } else {
-        document.querySelector('.scroll-thongdiep_content-chuyen h1').innerText = 'Các hệ thống tư duy'
-        document.querySelector('.scroll-thongdiep_content-chuyen .scroll-thongdiep_content-exp1').innerText = 'Hoạt động trong cuộc thi giúp tạo cơ hội được tìm hiểu và luyện tập hệ thống tư duy tốt.'
-        document.querySelector('.scroll-thongdiep_content-chuyen .scroll-thongdiep_content-exp2').innerText = '* Nhấn để xem thêm các hệ thống tư duy'
-        document.querySelector('.scroll-thongdiep_content1').innerHTML =
-            `<i class="bi bi-clipboard-check-fill"></i>
-                <h1>Tư duy logic</h1>
-            <p>Cách thức để xác định vấn đề đang xảy ra trong ngành khoa học.</p>`
-        document.querySelector('.scroll-thongdiep_content2').innerHTML =
-            `<i class="bi bi-clipboard-check-fill"></i>
-            <h1>Tư duy hệ thống</h1>
-            <p>Nâng cao khả năng nhìn nhận vấn đề một cách hệ thống, thông qua đó đưa ra các yêu cầu đầy đủ
-                và cần thiết để giải quyết các vấn đề được đặt ra một cách toàn diện.</p>`
-        document.querySelector('.scroll-thongdiep_content3').innerHTML =
-            ` <i class="bi bi-clipboard-check-fill"></i>
-            <h1>Tư duy thiết kế</h1>
-            <p>Nâng cao khả năng thiết kế sản phẩm một cách hiệu quả dựa trên yêu cầu thiết kế.</p>`
-        document.querySelector('.scroll-thongdiep_content4').innerHTML =
-            `<i class="bi bi-clipboard-check-fill"></i>
-            <h1>Tư duy sáng tạo</h1>
-            <p>Nâng cao khả năng đưa ra các ý tưởng sáng tạo nhằm phục vụ giải quyết các vấn đề về khoa học
-                cũng như cuộc sống thường ngày.</p>`
-    }
-}
+const vam = document.querySelector.bind(document);
+const vams = document.querySelectorAll.bind(document);
 
+/* #c661412b2 */
+vam('#c661412b2-iframe_click').addEventListener('click', () => {
+    vam('.c661412b2-background_click').setAttribute('style', 'display:flex')
+    vam('.c661412b2-iframe_click').setAttribute('style', 'display:flex')
+    var c661412b2Iframe = `<iframe  class="c661412b2-iframe" src="https://drive.google.com/file/d/1tfMlBjup692cFrsEFpX_x0Ehb0Fi7wAO/preview" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+    vam('.c661412b2-iframe_click').innerHTML = c661412b2Iframe
+    vam('.c661412b2-background_click').addEventListener('click', () => {
+        vam('.c661412b2-background_click').setAttribute('style', 'display:none')
+        vam('.c661412b2-iframe_click').setAttribute('style', 'display:none')
+        vam('.c661412b2-iframe').remove()
+    })
+})
 window.addEventListener("scroll", function (event) {
 
     var scroll_y = this.scrollY;
@@ -59,7 +27,7 @@ window.addEventListener("scroll", function (event) {
         document.querySelector('#header1412a1').setAttribute('style', `height: max-content;
         position: sticky;`)
         document.querySelector('#header_logo').src = 'https://lh3.googleusercontent.com/d/1UsHT-SsPNDizafj3sBICKJZXpvyY5zCm'
-        document.querySelector('.header1412a1-list').setAttribute('style', 'color:blue')
+        document.querySelector('.header1412a1-list').setAttribute('style', 'color:var(--color-text)')
 
 
     } else {
@@ -70,30 +38,18 @@ window.addEventListener("scroll", function (event) {
     }
 });
 
+window.onscroll = () => {
+    var scroll = document.querySelector('#timeline').getClientRects()[0];
+    console.log()
+    if (scroll.top < -80) {
+        document.querySelectorAll('.dot>div').forEach((t) => {
+            t.classList.add('start')
+        })
+    }
+};
+
 let w = screen.width
-if (w > 992) {
-    registerBtn.addEventListener('click', () => {
-        container.classList.add("active");
-        document.querySelector('.sign-in').setAttribute('style', 'display:none')
-    });
 
-    loginBtn.addEventListener('click', () => {
-        container.classList.remove("active");
-        document.querySelector('.sign-in').setAttribute('style', 'display:unset')
-    });
-} else {
-    registerBtn.addEventListener('click', () => {
-        container.classList.add("active");
-        document.querySelector('.sign-in').setAttribute('style', 'display:none')
-        document.querySelector('.toggle').setAttribute('style', 'left: -125%;')
-    });
-
-    loginBtn.addEventListener('click', () => {
-        container.classList.remove("active");
-        document.querySelector('.sign-in').setAttribute('style', 'display:unset')
-        document.querySelector('.toggle').setAttribute('style', 'left: -150%;')
-    });
-}
 
 fetchSheet
     .fetch({
@@ -152,67 +108,6 @@ fetchSheet
     });
 
 
-fetchSheet
-    .fetch({
-        gSheetId: '1w0ZWTWCwLovFMRhTHiYAS9yst4qptwjLRcI2GncXUjI',
-        wSheetName: 'timeline',
-    })
-    .then((rows) => {
-        let d = ""
-        let i = 1
-        let f = ''
-        document.querySelector('#timeline_titleup').innerText = rows[0]['Tiêu đề']
-        document.querySelector('#timeline_title').innerText = rows[0]['Tiêu đề']
-        rows.forEach((t) => {
-
-            if (w < 640) {
-                f +=
-                    `<div class="timeline_items">
-                        <div class="timeline_item-main l">
-                            <h2>${t.time}</h2>
-                            <div class="timeline_item-content">
-                                <p>${t.content}</p>
-                            </div>
-                            <span class="timeline_item-index l">${i}</span>
-                        </div>
-                    </div>`
-                i++
-            } else {
-                if (i % 2 === 0) {
-                    d +=
-                        `<div class="timeline_items">
-                        <div class="timeline_item-main l">
-                            <h2>${t.time}</h2>
-                            <div class="timeline_item-content">
-                                <p>${t.content}</p>
-                            </div>
-                            <span class="timeline_item-index l">${i}</span>
-                        </div>
-                    </div>`
-                } else {
-                    d +=
-                        `<div class="timeline_items">
-                        <div class="timeline_item-main c">
-                            <span class="timeline_item-index c">${i}</span>
-    
-                            <div class="timeline_item-content">
-                                <p>${t.content}</p>
-                            </div>
-                            <h2>${t.time}</h2>
-                        </div>
-                    </div>`
-                }
-                i++
-            }
-        })
-        if (w < 640) {
-            document.querySelector('#timeline_list-wrap').innerHTML = f
-        } else {
-            document.querySelector('#timeline_list-wrap').innerHTML = d
-        }
-
-    });
-
 jQuery(document).ready(function ($) {
     $(".slider-img").on("click", function () {
         $(".slider-img").removeClass("active");
@@ -226,8 +121,6 @@ fetchSheet
     })
     .then((rows) => {
         let r = ''
-        document.querySelector('#time_title').innerText = rows[0]['Tiêu đề']
-        document.querySelector('#iframevideo').src = rows[0]['iframe']
         const targetDate = new Date(`${rows[0]['Thời gian hết hạn']}T23:59:59`).getTime();
 
         // Update the countdown every second
